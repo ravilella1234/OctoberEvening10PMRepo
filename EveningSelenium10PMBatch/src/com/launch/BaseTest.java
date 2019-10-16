1aa7fca064e46d050415a1a13288b93a7012a3ec
ravilella1234 <ravilella1234>
2019-10-16 23:03:32 +0530

    Browser Commands

--- EveningSelenium10PMBatch/src/com/launch/BaseTest.java
+++ EveningSelenium10PMBatch/src/com/launch/BaseTest.java
@@ -1,5 +1,8 @@
 package com.launch;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,24 +11,39 @@ public class BaseTest
 {
 	
 	public static WebDriver driver;
+	public static String projectpath=System.getProperty("user.dir");
+	public static Properties p;
+	
+	
+	public static void init() throws Exception
+	{
+		FileInputStream fis=new FileInputStream(projectpath+"//data.properties");
+		p=new Properties();
+		p.load(fis);
+	}
 	
 	public static void launch(String browser)
 	{
-		if(browser.equals("chrome"))
+		if(p.getProperty(browser).equals("chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\drive\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\drive\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver",projectpath+"//drivers//chromedriver.exe" );
 			driver=new ChromeDriver();
 		}
-		else if(browser.equals("firefox")) 
+		else if(p.getProperty(browser).equals("firefox")) 
 		{
-			System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\drive\\geckodriver.exe");
+			//System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\drive\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", projectpath+"//drivers//geckodriver.exe" );
 			driver=new FirefoxDriver();
 		}
 	}
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(url);
+		//driver.get(p.getProperty(url));
+		
+		driver.navigate().to(p.getProperty(url));
+		
 	}
 	
 
--- EveningSelenium10PMBatch/src/com/launch/TC_002.java
+++ EveningSelenium10PMBatch/src/com/launch/TC_002.java
@@ -1,13 +1,43 @@
 package com.launch;
 
+import org.openqa.selenium.By;
+
 public class TC_002 extends BaseTest
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
-		launch("chrome");
+		init();
 		
-		navigateUrl("https://www.bestbuy.com");
+		launch("chromebrowser");
+		
+		navigateUrl("hdfcurl");
+		
+		driver.manage().window().maximize();
+		
+		/*String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().refresh();*/
+		
+		driver.findElement(By.linkText("APPLY ONLINE")).click();
+		
+		driver.close();
+		
+		//driver.quit();
 
 	}
 
